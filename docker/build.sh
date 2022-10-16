c73385f2f3ba103f3726d50b36f9d17a7f553ee0
shizacat <tippet@yandex.ru>
2022-10-16 19:02:47 +0300

    hmmm

--- docker/build.sh
+++ docker/build.sh
@@ -87,7 +87,9 @@ dpkg -i libwbxml2-0_0.11.8-1_amd64.deb libwbxml2-dev_0.11.8-1_amd64.deb
 apt-get -f install -y
 
 # Fix python environment
-ln -s /usr/bin/python3.8 /usr/bin/python
+if [[ ! -h /usr/bin/python ]]; then
+    ln -s /usr/bin/python3.8 /usr/bin/python
+fi
 
 # Build section
 # -------------
