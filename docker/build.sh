08f052a31275b285fc8ee27a81df880314a96249
Alexey <tippet@yandex.ru>
2022-10-17 00:14:21 +0300

    to v5.6.0 (#6)

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,7 @@ DEBFULLNAME="Christian Schweingruber"
 DEBEMAIL="c.schweingruber@catatec.ch"
 
 # Version of SOGo which will be built
-VERSION_TO_BUILD="5.5.1"
+VERSION_TO_BUILD="5.6.0"
 
 # Post config section
 # -------------------
